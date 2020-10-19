ebd256fdebaed2c1e435ffbc8563490c03691292
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-19 09:49:10 +0100

    first commit

---
+++ intro_to_express_start/client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>My App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted(){
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
